f43b0a4a6f141a3b671a185f63fcdb5660eeb8ab
Lucrezia <ateossg5@gmail.com>
2024-04-23 10:45:33 +0200

    first impl of Activity class and methods

---
+++ classes/impl.py
@@ -0,0 +1,59 @@
+# from sqlite3 import connect
+# from pandas import read_sql, DataFrame, concat, read_csv, Series, merge
+# from utils.paths import RDF_DB_URL, SQL_DB_URL
+# from rdflib import Graph, Namespace
+# from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
+# from sparql_dataframe import get 
+# from clean_str import remove_special_chars
+# from json import load
+# from utils.CreateGraph import create_Graph
+# from urllib.parse import urlparse
+
+class Activity(): # relation = Activity "refersTo" cho
+    def __init__(self, id, institute, person, tool, start, end):
+        self.institute = institute 
+        self.person = person
+        self.tool = set() 
+
+        if tool is not None:
+            if isinstance(tool, str):
+                self.tool.add(tool)  # If it's a single string (one tool), add it to the set
+            elif isinstance(tool, set):
+                self.tool.update(tool)  # If there are multiple tools, add all to the set
+
+        self.start = start
+        self.end = end
+
+    def getResponsibleInstitute(self):
+        return self.institute
+    
+    def getResponsiblePerson(self):
+        if self.person:
+            return self.person
+        else:
+            return None
+    
+    def getTools(self): # getTool has arity zero or more [0..*]
+        return self.tool
+    
+    def getStartDate(self):
+        if self.start:
+            return self.start
+        else:
+            return None
+
+    def getEndDate(self):
+        if self.end:
+            return self.end
+        else:
+            return None
+
+    def refersTo(self):
+        return self.CulturalHeritageObject
+    
+
+
+class Acquisition(Activity):
+    def __init__(self, id, institute, person, tool, start, end, technique: str):
+        super().__init__(id, institute, person, tool, start, end)
+        self.technique = technique
