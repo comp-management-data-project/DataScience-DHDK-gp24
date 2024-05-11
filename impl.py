ac9fdef23288657c6906de6a1b59073d7ce8f8f3
Iheb Ouerghi <146657705+ThIheb@users.noreply.github.com>
2024-05-11 15:45:16 +0200

    Updated impl.py

--- impl.py
+++ impl.py
@@ -166,6 +166,13 @@ class QueryHandler(Handler):
     def __init__(self, dbPathOrUrl=""):  # Provide a default value for dbPathOrUrl
         super().__init__()
         self.dbPathOrUrl = dbPathOrUrl
+          def getById(self, id):
+        idDataFrame = idDataFrame["id"]
+        self.getById = idDataFrame
+        return self.getById
+    def MetadataQueryHandler(QueryHandler):
+        def getAllPeople(self):
+            return
 
     def getById(self, id):
         return None
