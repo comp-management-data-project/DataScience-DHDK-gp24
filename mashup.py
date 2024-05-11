c636154f97050377c7b116d7f2644838bf9da6af
Lucrezia <ateossg5@gmail.com>
2024-05-11 17:36:22 +0200

    -

--- mashup.py
+++ mashup.py
@@ -59,25 +59,37 @@ class BasicMashup(object):  #Hubert
 
     # helper method to reduce code clutter # Hubert
     # column names here might have to be changed, depending on your implementation of loading data to sql
-    def createActivityList(self, df): 
-        activities = [];
-        for idx, row in df.iterrows():
-            if row["Activity_internal_id"].contains("acquisition"):
-                activity = impl.Acquisition(impl.CulturalHeritageObject(row["activity_internal_id"].rsplit('-'), "", "", "", "", ""), row["Responsible Institute"], row["Responsible Person"], row["Tool"], row["Start Date"], row["End Date"], row["Technique"]);
-                activities.append(activity);
-            if row["Activity_internal_id"].contains("processing"):
-                activity = impl.Processing(impl.CulturalHeritageObject(row["activity_internal_id"].rsplit('-'), "", "", "", "", ""), row["Responsible Institute"], row["Responsible Person"], row["Tool"], row["Start Date"], row["End Date"]);
-                activities.append(activity);
-            if row["Activity_internal_id"].contains("modelling"):
-                activity = impl.Modelling(impl.CulturalHeritageObject(row["activity_internal_id"].rsplit('-'), "", "", "", "", ""), row["Responsible Institute"], row["Responsible Person"], row["Tool"], row["Start Date"], row["End Date"]);
-                activities.append(activity);
-            if row["Activity_internal_id"].contains("optimising"):
-                activity = impl.Optimising(impl.CulturalHeritageObject(row["activity_internal_id"].rsplit('-'), "", "", "", "", ""), row["Responsible Institute"], row["Responsible Person"], row["Tool"], row["Start Date"], row["End Date"]);
-                activities.append(activity);
-            if row["Activity_internal_id"].contains("exporting"):
-                activity = impl.Exporting(impl.CulturalHeritageObject(row["activity_internal_id"].rsplit('-'), "", "", "", "", ""), row["Responsible Institute"], row["Responsible Person"], row["Tool"], row["Start Date"], row["End Date"]);
-                activities.append(activity);
-        return activities;
+    def createActivityList(self) -> list[impl.Activity]:
+        activities = []
+
+        if self.metadataQuery:
+            activities_data = self.metadataQuery.getAllActivities()
+            for idx, row in activities_data.iterrows():
+                activity_type = row["type"]
+                cultural_object = impl.CulturalHeritageObject(row["object id"], "", "", "", "")
+                institute = row[activity_type]["responsible institute"]
+                person = row[activity_type]["responsible person"]
+                tool = set(row[activity_type]["tool"])
+                start = row[activity_type]["start date"]
+                end = row[activity_type]["end date"]
+                technique = row[activity_type]["technique"] if activity_type == "acquisition" else ""
+
+                if activity_type == "acquisition":
+                    activity = impl.Acquisition(cultural_object, institute, person, tool, start, end, technique)
+                elif activity_type == "processing":
+                    activity = impl.Processing(cultural_object, institute, person, tool, start, end)
+                elif activity_type == "modelling":
+                    activity = impl.Modelling(cultural_object, institute, person, tool, start, end)
+                elif activity_type == "optimising":
+                    activity = impl.Optimising(cultural_object, institute, person, tool, start, end)
+                elif activity_type == "exporting":
+                    activity = impl.Exporting(cultural_object, institute, person, tool, start, end)
+                else:
+                    continue  # Skip unknown activity types
+
+                activities.append(activity)
+
+        return activities
 
     # helper method to reduce code clutter
     # column names here might have to be changed, depending on your implementation of loading data to sparql
